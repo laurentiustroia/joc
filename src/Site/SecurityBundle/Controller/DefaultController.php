2d28a5d472b5c15c7dc48c9a6ea84813abf9fad9
Laurentiu <stroia.laurentiu92@gmail.com>
2014-12-06 21:33:11 +0200

    create bundle security

---
+++ src/Site/SecurityBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Site\SecurityBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('SiteSecurityBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
---
+++ src/Site/SecurityBundle/SiteSecurityBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Site\SecurityBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class SiteSecurityBundle extends Bundle
+{
+}
